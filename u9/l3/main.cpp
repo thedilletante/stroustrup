ecb143a12e4d2a443941160388e78c4fee082032
Pokhikhilov Mikhail <thedilletante@gmail.com>
2014-01-31 09:30:24 +0400

    clear names to output

--- u9/l3/main.cpp
+++ u9/l3/main.cpp
@@ -140,7 +140,7 @@ Token_value getToken() {
 std::map<std::string, unsigned> includies;
 std::string sys_root = "/usr/include/c++/4.8.2/";
 
-std::string find_file(const std::string& directive) {
+std::pair<std::string, std::string> find_file(const std::string& directive) {
 	std::string include("include");
 	size_t pos = directive.find(include);
 	if ( pos != std::string::npos ) {
@@ -156,11 +156,12 @@ std::string find_file(const std::string& directive) {
 			pos = arg.find(stop, 1);
 			if (pos != std::string::npos) {
 				std::string name = arg.substr(1, pos - 1);
-				return ((stop == '"') ? "./" : sys_root ) + name;
+				std::string path = ((stop == '"') ? "./" : sys_root ) + name;
+				return std::pair<std::string, std::string>(path,name);
 			}
 		}
 	}
-	return "";
+	return std::pair<std::string, std::string>("","");
 }
 
 
@@ -170,21 +171,22 @@ void scan(const std::string& filename, unsigned level = 0) {
 	if (file.is_open()) {
 		std::istream* tmp = input;
 		input = &file;
-		typedef std::vector<std::string> vstring;
+		typedef std::pair< std::string, std::string > pstring;
+		typedef std::vector< pstring > vstring;
 		vstring depends;
-		std::string name;
+		pstring name;
 		while(getToken() != END) {
 			if (curr_tok == PREPROC_DIRECTIVE) {
 				name = find_file(value);
-				if (!name.empty()) depends.push_back(name);
+				if (!name.first.empty()) depends.push_back(name);
 			}
 		}
 		input = tmp;
 		for (vstring::iterator iter = depends.begin(), end = depends.end(); iter != end; ++iter) {
 			for (unsigned i = 0; i < level; ++i) std::cout << "\t";
-			std::cout << *iter << "\n";
-			if ( includies[ *iter ]++ == 0 ) {
-				scan( *iter, level + 1 );
+			std::cout << iter->second << "\n";
+			if ( includies[ iter->first ]++ == 0 ) {
+				scan( iter->first, level + 1 );
 			}
 		}
 	}
